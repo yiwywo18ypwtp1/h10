a590d396015ad08a5c9f3d31b37e2a0d381ddd05
Alexander Ivanitskiy <negalapetrovna77@gmail.com>
2025-01-27 16:13:43 +0200

    all pages done

--- hw10/hw10/urls.py
+++ hw10/hw10/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 
 from quotes import views
 
+ajink
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', views.main_page, name='main_page'),
+    path('<str:tag>/', views.main_page, name='main_page'),
+    path('author/<str:name>/', views.author, name='author'),
+    path('login', views.login, name='login'),
 ]
--- hw10/quotes/views.py
+++ hw10/quotes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from quotes.models import Quote
+from quotes.models import Writer
 
 
 def main_page(request):
@@ -8,8 +9,30 @@ def main_page(request):
     quotes_list = []
 
     for i in all_quotes:
-        print(f'{i.writer.name}: - {i.text} | {i.tag}')
+        # print(f'{i.writer.name}: - {i.text} | {i.tag}')
         quotes_list.append(i)
 
-    context = quotes_list
-    return render(request, 'index.html', context={'quotes': all_quotes})
+    context = {
+        'quotes': quotes_list,
+    }
+    return render(request, 'index_page.html', context=context)
+
+
+def author(request, name):
+    all_writers = Writer.objects.all()
+    writer = ''
+    for i in all_writers:
+        if i.name == name:
+            writer = i
+            # print(f'writer = {writer}')
+            break
+
+    context = {
+        'author_name': name,
+        'writer': writer
+    }
+    return render(request, 'author_page.html', context=context)
+
+
+def login(request):
+    return render(request, 'login_page.html')
